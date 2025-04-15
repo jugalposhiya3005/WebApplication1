fb7960f7a3ed71ddcb71442d5facd861686575e3
SERVER7\pct152 <jugal.poshiya@tatvasoft.com>
2025-04-15 18:16:28 +0530

    Test CI/CD: update summary message

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "CI/CD Success", "Bracing", "Chilly Potato", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "CI/CD Success", "Bracing", "Chilly Potato", "Cool", "Mild", "Warm", "Balmy", "Meggi", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
