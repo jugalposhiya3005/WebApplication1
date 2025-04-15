4636b02d883d053ec2971b5714f2da6fa23dabe6
SERVER7\pct152 <jugal.poshiya@tatvasoft.com>
2025-04-15 16:46:48 +0530

    Deploy to EC2

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.....
+
     app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApp v1"));
 
